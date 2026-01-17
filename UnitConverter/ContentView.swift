304b36eaeaffc9a759a5d186ff93c8260f1b86a4
Brittany Pollock <brittpollock15@gmail.com>
2026-01-17 11:38:19 -0800

    Initial Commit

---
+++ UnitConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by Brittany Pollock on 1/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Brittany Pollock on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
